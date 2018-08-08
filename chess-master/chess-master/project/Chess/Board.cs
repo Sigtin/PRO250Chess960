f5c7c9636d648f13a36f85596a36e700b4103091
Unknown <asigler@student.neumont.edu>
2018-08-07 21:58:36 -0600

    Chess960 Works. Time for Unit Tests

--- chess-master/chess-master/project/Chess/Board.cs
+++ chess-master/chess-master/project/Chess/Board.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Chess
 {
@@ -159,14 +160,75 @@ namespace Chess
 
         public void SetChess960Placement()
         {
+            List<int> availableSpaces = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7 };
             for (int i = 0; i < 8; i++)
             {
                 SetPiece(Piece.PAWN, Player.WHITE, i, 1);
                 SetPiece(Piece.PAWN, Player.BLACK, i, 6);
             }
-
+            
             Random rand = new Random();
 
+            int bishop1Index = rand.Next(0, availableSpaces.Count);
+            int bishop1Num = availableSpaces[bishop1Index];
+            SetPiece(Piece.BISHOP, Player.WHITE, bishop1Num, 0);
+            SetPiece(Piece.BISHOP, Player.BLACK, bishop1Num, 7);
+            availableSpaces.RemoveAt(bishop1Index);
+
+            bool isValidBishop = false;
+
+            while (isValidBishop == false)
+            {
+                int bishop2Index = rand.Next(0, availableSpaces.Count);
+                int bishop2Num = availableSpaces[bishop2Index];
+                if(bishop1Num % 2 != bishop2Num % 2)
+                {
+                    isValidBishop = true;
+                    SetPiece(Piece.BISHOP, Player.WHITE, bishop2Num, 0);
+                    SetPiece(Piece.BISHOP, Player.BLACK, bishop2Num, 7);
+                    availableSpaces.RemoveAt(bishop2Index);
+                }
+            }
+
+            int knight1Index = rand.Next(0, availableSpaces.Count);
+            int knight1Num = availableSpaces[knight1Index];
+            SetPiece(Piece.KNIGHT, Player.WHITE, knight1Num, 0);
+            SetPiece(Piece.KNIGHT, Player.BLACK, knight1Num, 7);
+            availableSpaces.RemoveAt(knight1Index);
+
+            int knight2Index = rand.Next(0, availableSpaces.Count);
+            int knight2Num = availableSpaces[knight2Index];
+            SetPiece(Piece.KNIGHT, Player.WHITE, knight2Num, 0);
+            SetPiece(Piece.KNIGHT, Player.BLACK, knight2Num, 7);
+            availableSpaces.RemoveAt(knight2Index);
+
+            int queenIndex = rand.Next(0, availableSpaces.Count);
+            int queenNum = availableSpaces[queenIndex];
+            SetPiece(Piece.QUEEN, Player.WHITE, queenNum, 0);
+            SetPiece(Piece.QUEEN, Player.BLACK, queenNum, 7);
+            availableSpaces.RemoveAt(queenIndex);
+
+            int rook1Index = 0;
+            int rook1Num = availableSpaces[rook1Index];
+
+            int kingIndex = 1;
+            int kingNum = availableSpaces[kingIndex];
+
+            int rook2Index = 2;
+            int rook2Num = availableSpaces[rook2Index];
+
+            SetPiece(Piece.ROOK, Player.WHITE, rook1Num, 0);
+            SetPiece(Piece.ROOK, Player.BLACK, rook1Num, 7);
+            SetPiece(Piece.ROOK, Player.WHITE, rook2Num, 0);
+            SetPiece(Piece.ROOK, Player.BLACK, rook2Num, 7);
+
+            Kings[Player.WHITE] = new position_t(kingNum, 0);
+            Kings[Player.BLACK] = new position_t(kingNum, 7);
+            SetPiece(Piece.KING, Player.WHITE, kingNum, 0);
+            SetPiece(Piece.KING, Player.BLACK, kingNum, 7);
+            availableSpaces.Remove(kingNum);
+            availableSpaces.Remove(rook1Num);
+            availableSpaces.Remove(rook2Num);
         }
 
         public void SetPiece(Piece piece, Player player, int letter, int number)
