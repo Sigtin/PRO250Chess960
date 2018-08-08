971583812b3bf952c3fddf8b8dba051e479c9b88
Unknown <asigler@student.neumont.edu>
2018-08-08 15:40:27 -0600

    Last Two Unit Tests

--- chess-master/chess-master/project/ChessTests/UnitTest1.cs
+++ chess-master/chess-master/project/ChessTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Chess;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -33,6 +34,46 @@ namespace ChessTests
         {
             board.SetPiece(Piece.PAWN, Player.WHITE, 0, 1);
             if (board.Grid[1][0].player != Player.WHITE)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void AssertThatBishopsAreOnDifferentColoredSquares()
+        {
+            board.SetChess960Placement();
+            List<int> bishopIndices = new List<int>();
+            
+            for (int i = 0; i < board.Grid[0].Length; i++)
+            {
+                if(board.Grid[0][i].piece == Piece.BISHOP)
+                {
+                    bishopIndices.Add(i);
+                }
+            }
+
+            if(bishopIndices[0] % 2 == bishopIndices[1] % 2)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void AssertThatKingIsBetweenRooks()
+        {
+            board.SetChess960Placement();
+            List<int> pieceIndices = new List<int>();
+
+            for (int i = 0; i < board.Grid[0].Length; i++)
+            {
+                if (board.Grid[0][i].piece == Piece.ROOK || board.Grid[0][i].piece == Piece.KING)
+                {
+                    pieceIndices.Add(i);
+                }
+            }
+
+            if (!(pieceIndices[1] < pieceIndices[2] && pieceIndices[1] > pieceIndices[0]))
             {
                 Assert.Fail();
             }
